c565a5980321c1e0643972ec7867267e41d48e4e
gurimmer <s.reianai@gmail.com>
2018-01-21 06:15:01 +0900

    add Dollar class

---
+++ src/app/Dollar.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+
+class Dollar
+{
+    public $amount;
+
+    public function __construct(int $amount)
+    {
+        $this->amount = $amount;
+    }
+
+    public function times(int $multiplier): void
+    {
+        $this->amount *= $multiplier;
+    }
+
+}
